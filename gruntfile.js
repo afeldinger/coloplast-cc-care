c037fc64e437af2b6a9021ccb2c35545014eb3e7
Anders Feldinger <af@andersfeldinger.dk>
2015-03-05 11:44:28 +0100

    added forms

--- gruntfile.js
+++ gruntfile.js
@@ -308,7 +308,7 @@ module.exports = function(grunt) {
           {
             expand: true, 
             flatten: true, 
-            src: ['dist/**/*.{html,css}'], 
+            src: ['dist/**/*.{html,css}'],
             dest: 'dist/'
           }
         ]
