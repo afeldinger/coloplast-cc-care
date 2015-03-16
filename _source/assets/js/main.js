14556c969a55abb0c6033ba924b0a38b6cb73437
Anders Feldinger <af@andersfeldinger.dk>
2015-03-16 22:50:55 +0100

    added forms styling

--- _source/assets/js/main.js
+++ _source/assets/js/main.js
@@ -71,7 +71,6 @@ function grid_list_init() {
 
 		// bind filter function to change events
 		$(this).find('input[name^="filter"]').change(function() {
-
 			var filters = [];
 			$('input[name^="filter"]:checked').each(function() {
 				if (filters[this.name] === undefined) {
@@ -80,7 +79,7 @@ function grid_list_init() {
 				filters[this.name].push('.' + this.name + '--' + this.value);
 			});
 
-			targets.hide().removeClass('last').filter(function() {
+			targets.hide().filter(function() {
 				var visible = true;
 				for(var k in filters) {
 					var filter_str = filters[k].join(',');
@@ -101,10 +100,8 @@ function grid_list_init() {
 			}
 			$('#show-categories-count').html((filter_count>0)? '('+filter_count+')' : '');
 
-
 		});
-		
-	});
+	}).change();
 	grid_list_sort();
 }
 
@@ -122,13 +119,16 @@ $(document).ready(function() {
 	    }
 	});
 */
+	
 	grid_list_init();
 
+	// init flexsliders on page
 	$('.flexslider').flexslider({
 		animation: 'slide',
 		slideshow: false,
 	});
 
+	// set hover classes in navigation
 	$('#primary-nav')
 		.find('li').hover(
 			function() {
@@ -139,25 +139,40 @@ $(document).ready(function() {
 			}
 		)
 	;
+
+	// init responsive navigation
 	$('.nav-btn a').click(function(e) {
 		e.preventDefault();
 		$(this).add($('#primary-nav')).toggleClass('active');
 	});
 
 
-	$("input[type=radio], input[type=checkbox]").each(function() {
+	// control checkboxes and radiobuttons
+	$('input[type=radio], input[type=checkbox]').each(function() {
 
 		// set state on load
-		$(this).parents("label").toggleClass("checked", $(this).is(":checked"));
+		$(this).parents('label').toggleClass('checked', $(this).is(':checked'));
 
 		$(this).on('change', function() {
 
-	        var fld_name = $(this).attr("name");
-            $("input[name=" + fld_name + "]").add(this).each(function() {
-                $(this).parents("label").toggleClass("checked", $(this).is(":checked"));
+	        var fld_name = $(this).attr('name');
+            $('input[name=' + fld_name + ']', $(this).parents('form:first')).add(this).each(function() {
+                $(this).parents('label').toggleClass('checked', $(this).is(':checked'));
             });
 		});
 	});
+
+	// control input fields focus/blur state
+	$(':input').focus(function() {
+		$(this).parents('label').addClass('focus');
+	}).blur(function() {
+
+		$(this).parents('label')
+			.removeClass('focus')
+			.toggleClass('has-value', $(this).val()!=='')
+		;
+	});
+
 
 });
 
