089754b7cec55b443e68b97ba9d1f75c9e4c08ba
Anders Feldinger <af@andersfeldinger.dk>
2015-04-30 12:14:13 +0200

    image lightness detection

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -3318,9 +3318,58 @@ $.leftPad = function(i,l,s) {
 	return o;
 };
 
+function getImageLightness(imageSrc, callback, target) {
+    var img = document.createElement("img");
+    img.src = imageSrc;
+    img.style.display = "none";
+    document.body.appendChild(img, callback);
+
+    var colorSum = 0;
+
+    img.onload = function() {
+        // create canvas
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this,0,0);
+
+        var imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
+        var data = imageData.data;
+        var r,g,b,avg;
+
+        for(var x = 0, len = data.length; x < len; x+=4) {
+            r = data[x];
+            g = data[x+1];
+            b = data[x+2];
+
+            avg = Math.floor((r+g+b)/3);
+            colorSum += avg;
+        }
+
+        var brightness = Math.floor(colorSum / (this.width*this.height));
+        callback(brightness, target);
+    }
+}
+
 $(document).ready(function() {
 
 	grid_list_init();
+
+
+	$('.hero, .article-header').filter(':has(.elm-image img)').each(function() {
+		var src = $(this).find('.elm-image img').attr('src');
+		getImageLightness(src, function(brightness, target) {
+			console.log('lightness', src, brightness, target);
+			var threshold = 155;
+			target.toggleClass('background-light', (brightness > threshold));
+			target.toggleClass('background-dark', (brightness <= threshold));
+
+		}, $(this));
+	});
+
+	
 
 	// init flexsliders on page
 	$('.flexslider').flexslider({
